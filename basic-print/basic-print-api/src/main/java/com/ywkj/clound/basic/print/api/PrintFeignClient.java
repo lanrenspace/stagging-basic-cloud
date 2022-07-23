94dee1ff91aaf5a58be7285c54f481952b31a177
helongjun <lanrenspace@163.com>
2022-07-23 12:09:12 +0800

    bulid:修改fegin配置地址

--- basic-print/basic-print-api/src/main/java/com/ywkj/clound/basic/print/api/PrintFeignClient.java
+++ basic-print/basic-print-api/src/main/java/com/ywkj/clound/basic/print/api/PrintFeignClient.java
@@ -6,7 +6,7 @@ import com.ywkj.cloud.basic.print.dto.PrintDataDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(value = "${api.feign.client.uum}", path = "/basic/feign/print", contextId = "printFeignClient")
+@FeignClient(value = "${api.feign.client.print}", path = "/basic/feign/print", contextId = "printFeignClient")
 public interface PrintFeignClient {
 
     /**
